906b851cc478df327ce4c2dfda2e978ebd2a07b8
nineclue <nineclue@gmail.com>
2025-03-13 14:29:48 +0900

    create and destroy separate WebviewWindow; added quit menu item

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-const { app, tray, menu } = window.__TAURI__;
+const { app, tray, menu, webviewWindow, process } = window.__TAURI__;
 
 app.getName().then((appName) => {
-    console.log("hello, ".concat(appName));
+    console.log("hello, " + appName);
 });
 
 const tauriIcon = await app.defaultWindowIcon();
+const targetLabel = "targetWebviewWindow";
+
 function menuHandler(id) {
     console.log(id);
     var url;
@@ -18,15 +20,43 @@ function menuHandler(id) {
         default:
             url = "https://google.com";
     }
-    window.location.replace(url);
+    // window.location.replace(url);
+    console.log('Loading... ' + url);
+    webviewWindow.WebviewWindow.getByLabel(targetLabel).then(function(wvwOrNull) {
+        if (wvwOrNull == null) {
+            console.log("Target WebviewWindow doesn't exist.");
+            createNewWebViewWindow(url);
+        } else {
+            console.log("Destroying existing target WebviewWindow");
+            wvwOrNull.destroy().then(function () {
+                createNewWebViewWindow(url);
+            }, function(e) {
+                console.log("destroy failed" + e);
+            })
+        }        
+    });
+}
+
+function createNewWebViewWindow(url) {
+    console.log("creating new Target WebviewWindow.");
+    let wvw = new webviewWindow.WebviewWindow(targetLabel, { "url": url });
+    wvw.once("tauri://webview-created", function () {
+        console.log("Target WebviewWindow created... Now showing");
+        wvw.show();
+    }).then(function (unlistener) {
+        console.log("unlistening");
+        unlistener();
+    });
 }
 
 const tauriMenu = await menu.Menu.new({
     items: [
         { id: "tauri", text: "Tauri", action: menuHandler },
         { id: "scalajs", text: "Scala.js", action: menuHandler },
+        { id: "quit", text: "Quit", action: function() { process.exit(0); } }
     ],
 });
+
 
 function trayHandler(e) {
     console.log(e);
@@ -38,6 +68,6 @@ const options = {
     menu: tauriMenu,
 };
 
-console.log("calling trayicon!");
 const t = await tray.TrayIcon.new(options);
-console.log("called trayicon!");
+
+export { menuHandler };
